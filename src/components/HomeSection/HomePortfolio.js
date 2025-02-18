2170b7f01d1857d6e58f1d56ddf39b8c64db18bf
IjjazAhmad <ijjazahmad705@gmail.com>
2025-02-18 21:22:02 +0500

    portfolio set

--- src/components/HomeSection/HomePortfolio.js
+++ src/components/HomeSection/HomePortfolio.js
@@ -1,22 +1,60 @@
 import React from "react";
 import images from "../../images";
 import ProjectShowCase from "../ProjectShowCase/ProjectShowCase";
-const Ecom = [
+const kalbosoft = [
   {
-    title: "E-commerce",
+    title: "Kalbosoft WebSite Home Page",
     description:
-      "I built this e-commerce website using React, Redux, and Firebase. It's a full-stack application that allows users to create, read, update, and delete products, as well as manage their orders.",
-    img: images.movieweb,
-    category: "Ecommerce Bussines",
+      "I built this kalbosoft Home Page website using next js, tailwind css. It's a full-stack application that allows users to create, read, update, and delete products, as well as manage their orders.",
+    img: images.kalbosoft_1,
+    category: "Agency Website",
     duration: "1-20 Days",
     cost: "$300-$350 Days",
   },
   {
-    title: "E-commerce About",
+    title: "Kalbosoft WebSite About Page",
     description:
-      "I built this e-commerce website using React, Redux, and Firebase. It's a full-stack application that allows users to create, read, update, and delete products, as well as manage their orders.",
-    img: images.todoapp,
-    category: "Ecommerce Bussines",
+      "I built this kalbosoft About Page website using next js, tailwind css. It's a full-stack application that allows users to create, read, update, and delete products, as well as manage their orders.",
+    img: images.kalbosoft_2,
+    category: "Agency Website",
+    duration: "1-20 Days",
+    cost: "$300-$350 Days",
+  },
+  {
+    title: "Kalbosoft WebSite Cases Page",
+    description:
+      "I built this kalbosoft Cases Page website using next js, tailwind css. It's a full-stack application that allows users to create, read, update, and delete products, as well as manage their orders.",
+    img: images.kalbosoft_3,
+    category: "Agency Website",
+    duration: "1-20 Days",
+    cost: "$300-$350 Days",
+  },
+];
+const orderUk = [
+  {
+    title: "Order.Uk WebSite Home Page",
+    description:
+      "I built this Order.Uk Home Page website using next js, tailwind css. It's a full-stack application that allows users to create, read, update, and delete products, as well as manage their orders.",
+    img: images.orderUk_1,
+    category: "Food Website",
+    duration: "1-20 Days",
+    cost: "$300-$350 Days",
+  },
+  {
+    title: "Order.Uk WebSite Resturant Page",
+    description:
+      "I built this Order.Uk Resturant Page website using next js, tailwind css. It's a full-stack application that allows users to create, read, update, and delete products, as well as manage their orders.",
+    img: images.orderUk_2,
+    category: "Food Website",
+    duration: "1-20 Days",
+    cost: "$300-$350 Days",
+  },
+  {
+    title: "Order.Uk WebSite Order Page",
+    description:
+      "I built this Order.Uk Order Page website using next js, tailwind css. It's a full-stack application that allows users to create, read, update, and delete products, as well as manage their orders.",
+    img: images.orderUk_3,
+    category: "Food Website",
     duration: "1-20 Days",
     cost: "$300-$350 Days",
   },
@@ -28,8 +66,8 @@ export default function HomePortfolio() {
         <span data-aos="fade-left">Who Am I ?</span>
         <h3 className="text-warning">Lastest Projects</h3>
       </div>
-      <ProjectShowCase projects={Ecom} />
-      <ProjectShowCase projects={Ecom} />
+      <ProjectShowCase projects={kalbosoft} />
+      <ProjectShowCase projects={orderUk} />
     </div>
   );
 }
