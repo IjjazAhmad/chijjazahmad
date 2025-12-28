3a9ff3cdba49643c1cb1570963fc3ccd9e9cf792
aqibabc <aqibkhanpuriya@gmail.com>
2025-12-28 20:14:01 +0500

    add change

--- src/pages/Frontend/Project/Project.js
+++ src/pages/Frontend/Project/Project.js
@@ -20,20 +20,6 @@ export default function Project() {
       
       <section className="portfolio__content py-5">
         <div className="container py-5">
-          <div className="row mb-5">
-            <div className="col-12 text-center">
-              <div className="section__label" data-aos="fade-up">
-                &lt; complete_portfolio /&gt;
-              </div>
-              <h2 className="section__title" data-aos="fade-up" data-aos-delay="100">
-                All Projects
-              </h2>
-              <p className="section__subtitle" data-aos="fade-up" data-aos-delay="200">
-                A comprehensive collection of web and mobile applications
-              </p>
-            </div>
-          </div>
-          
           {projects.slice(0, visibleProjects).map((projectGroup, index) => (
             <div className="row mb-4" key={index} data-aos="fade-up" data-aos-delay={index * 50}>
               <ProjectShowCase projects={projectGroup} />
